a6ad1b5e22c233157f7c9c47dceb485dd95f5cd0
Abhishek <abhishekthapliyal7579@gmail.com>
2019-02-27 13:32:21 +0530

    #1

---
+++ RxSwiftNew/RxSwiftNew/ImageCollage/ImageCollageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ImageCollageViewController.swift
+//  RxSwiftNew
+//
+//  Created by Abhishek Thapliyal on 27/02/19.
+//  Copyright © 2019 Abhishek Thapliyal. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class ImageCollageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func clearAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func saveAction(_ sender: UIButton) {
+        
+    }
+
+}
